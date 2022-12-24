4d22e430f89feee9972f0f8ffe61477b6eb1fa86
Gian <gian.defaria@hotmail.com>
2022-12-24 17:16:45 -0300

    adocopmamdo uma função que pegará os dados do array e adicionará no html elemento

--- src/components/input-list/input-list.tsx
+++ src/components/input-list/input-list.tsx
@@ -3,3 +3,15 @@
 import '../json-file/data';
 
 const list = document.querySelector('.list');
+
+//aqui estou criando uma função, que receberá como parâmetros um array e um elemento do html, onde será introduzido a lista
+const addJobList = (array: Array<any>, element: HTMLElement) => {
+    array.forEach(item => {
+        //atribuo li a criar um novo elemento li e indico o classname desse li além do item que ela conterá
+        const li = document.createElement('li');
+        li.className = "list-item";
+    })
+
+} 
+
+console.log('importado');
