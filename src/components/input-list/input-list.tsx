ab66f363cdbb556ff99f7e29bc2cfd5c641004cb
Gian <gian.defaria@hotmail.com>
2022-12-24 17:19:45 -0300

    adiciono texcontent que inicialmente será o nome da compania. E indico que dentro do elemento que será informado como parâmetro, será criado um novo elemento dentro dele, indicando que será uma li

--- src/components/input-list/input-list.tsx
+++ src/components/input-list/input-list.tsx
@@ -8,8 +8,10 @@ const list = document.querySelector('.list');
 const addJobList = (array: Array<any>, element: HTMLElement) => {
     array.forEach(item => {
         //atribuo li a criar um novo elemento li e indico o classname desse li além do item que ela conterá
-        const li = document.createElement('li');
+        const li: HTMLElement = document.createElement('li');
         li.className = "list-item";
+        li.textContent = item.company; //aqui estou indicando, inicialmente, que conterá o nome da compania dentro
+        element.appendChild(li); //cria um novo elemento, que sera uma li
     })
 
 } 
