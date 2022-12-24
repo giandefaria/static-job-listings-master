ee0b5113b1c1dfe35d455a5dd50229bc7ad36b25
Gian <gian.defaria@hotmail.com>
2022-12-24 13:52:38 -0300

    configurei o app para renderizar o que está dentro de main.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Principal from './components/main/main';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -11,7 +10,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
-    <Principal />
   </React.StrictMode>
 );
 
