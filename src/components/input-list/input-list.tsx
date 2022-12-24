12ca67d4ad98c5854bbdce15e544dfc797e5080c
Gian <gian.defaria@hotmail.com>
2022-12-24 17:21:58 -0300

    correção

--- src/components/input-list/input-list.tsx
+++ src/components/input-list/input-list.tsx
@@ -7,8 +7,9 @@ const list = document.querySelector('.list');
 //aqui estou criando uma função, que receberá como parâmetros um array e um elemento do html, onde será introduzido a lista
 const addJobList = (array: Array<any>, element: HTMLElement) => {
     array.forEach(item => {
-        //atribuo na const li a criação de um novo elemento html que será do tipo li.
-        li.className = "list-item"; //adiciono nome da classe dessa Li
+        //atribuo na const li a criação de um novo elemento html que será do tipo li
+        const li: HTMLElement = document.createElement('li');
+        li.className = "list-item"; //adiciono a classe que terá esse novo elemento
         li.textContent = item.company; //aqui estou indicando, inicialmente, que conterá o nome da compania dentro
         element.appendChild(li); //cria um novo elemento, que sera uma li
     })
