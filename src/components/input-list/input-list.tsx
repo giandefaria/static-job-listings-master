9b7533c0974dfc4c120f82dfd631440be3f26d48
Gian <gian.defaria@hotmail.com>
2022-12-25 20:59:05 -0300

    criei um for dentro de cada map, onde verificará a quantidade de languages dentro do array do map atual, e adicionará dentro de uma section criada

--- src/components/input-list/input-list.tsx
+++ src/components/input-list/input-list.tsx
@@ -23,15 +23,29 @@ export const AddJobList = (array: Array<any>, element: HTMLElement) => {
                     </div>    
                 </div>
             </section>
-            <section class="tags">
-            </section
         ` //estou adicionando uma template string no innerHTML
         
-        
+
         //li.innerHTML = itemArray.company; //aqui estou indicando, inicialmente, que conterá o nome da compania dentro
-        element.appendChild(li); //cria um novo elemento, que sera uma li
-        console.log('repete')
-    
+        element.appendChild(li); //cria um novo elemento dentro da section com a classe list, que sera uma li
+
+        //agora vou criar uma section dentro de li que conterá as tags
+        const creatSection = document.createElement('section');
+        creatSection.className = "tags";
+        li.appendChild(creatSection);
+        
+        //aqui eu adiciono os itens constantes no array language dentro da section criada
+        //para cada item será executado um loop
+        for (let i = 0; i < itemArray.languages.length; i++) {
+
+            console.log(itemArray.languages[i]);
+            const creatP = document.createElement('p');
+            creatP.textContent = itemArray.languages[i]; //é criado um <p> com o ítem do indice do loop
+            creatSection.appendChild(creatP)
+
+        }
+
+
         //add função verifNew e verifFeatured, que buscará no array constante na data de New e Featured são true ou false
         //se forem true, retornarão uma template string com o respectivo elemento html, com a classe e conteúdo, do contrário o retorno será em branco.
         function verifNew() {
@@ -52,7 +66,8 @@ export const AddJobList = (array: Array<any>, element: HTMLElement) => {
                 return(``)
             }
         }
-           
+
+              
     })//final do array.map
 }//final da arrow function da const AddJobList
 
