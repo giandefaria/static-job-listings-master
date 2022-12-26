74762232df8ec26356814e6b3454573564ce5cf8
Gian <gian.defaria@hotmail.com>
2022-12-26 00:43:09 -0300

    adicionando comentários

--- src/components/input-list/input-list.tsx
+++ src/components/input-list/input-list.tsx
@@ -9,8 +9,8 @@ export const AddJobList = (array: Array<any>, element: HTMLElement) => {
         console.log(itemArray);
         li.className = "list-item"; //adiciono a classe que terá esse novo elemento
         li.id = itemArray.id;
-        li.innerHTML = 
-        `
+        li.innerHTML =
+            `
             <section class="job-information">
                 <div>
                     <img src="${itemArray.logo}" alt="logo" />
@@ -23,13 +23,12 @@ export const AddJobList = (array: Array<any>, element: HTMLElement) => {
                     </div>    
                 </div>
             </section>
-        ` //estou adicionando uma template string no innerHTML
-        
+            ` //estou adicionando uma template string no innerHTML
 
-        //li.innerHTML = itemArray.company; //aqui estou indicando, inicialmente, que conterá o nome da compania dentro
-        element.appendChild(li); //cria um novo elemento dentro da section com a classe list, que sera uma li
 
-        //agora vou criar uma section dentro de li que conterá as tags
+        element.appendChild(li); //element = a section indicada pela classe list. Cria um novo elemento dentro da section, que sera uma li
+
+        //agora vou criar uma section dentro de li que conterá as tags que serão usadas para filtrar as informações
         const creatSection = document.createElement('section');
         creatSection.className = "tags";
         li.appendChild(creatSection);
@@ -43,12 +42,12 @@ export const AddJobList = (array: Array<any>, element: HTMLElement) => {
         const creatPLevel = document.createElement('p')
         creatPLevel.textContent = itemArray.level;
         creatSection.appendChild(creatPLevel);
-        
+
         //aqui eu adiciono os itens constantes no array language dentro da section criada
         //para cada item será executado um loop
         for (let i = 0; i < itemArray.languages.length; i++) {
-            console.log(itemArray.languages[i]);        
-            const creatPLanguages = document.createElement('p');            
+            console.log(itemArray.languages[i]);
+            const creatPLanguages = document.createElement('p');
             creatPLanguages.textContent = itemArray.languages[i]; //é criado um <p> com o ítem do indice do loop          
             creatSection.appendChild(creatPLanguages)
 
@@ -56,37 +55,34 @@ export const AddJobList = (array: Array<any>, element: HTMLElement) => {
 
         //agora um for para adicionar as ferramentas exigidas pela vaga, constantes no campo "tools" do array
         for (let i = 0; i < itemArray.tools.length; i++) {
-            console.log(itemArray.tools[i]);        
-            const creatPTools = document.createElement('p');            
+            console.log(itemArray.tools[i]);
+            const creatPTools = document.createElement('p');
             creatPTools.textContent = itemArray.tools[i]; //é criado um <p> com o ítem do indice do loop          
             creatSection.appendChild(creatPTools);
-
         }
-
-
 
         //add função verifNew e verifFeatured, que buscará no array constante na data de New e Featured são true ou false
         //se forem true, retornarão uma template string com o respectivo elemento html, com a classe e conteúdo, do contrário o retorno será em branco.
         function verifNew() {
             if (itemArray.new == true) {
                 return (
-                 `<span class="new">NEW!</span>`  
+                    `<span class="new">NEW!</span>`
                 )
-                                
+
             } else {
                 return (``)
             }
         }
-        
+
         function verifFeatured() {
             if (itemArray.featured == true) {
                 return (`<span class="featured">FEATURED</span>`)
             } else {
-                return(``)
+                return (``)
             }
         }
 
-              
+
     })//final do array.map
 }//final da arrow function da const AddJobList
 
