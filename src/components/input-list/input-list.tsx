db5b1eb7b5ddfcb83785cef4a6ed188fdfb9b74e
Gian <gian.defaria@hotmail.com>
2022-12-26 00:40:34 -0300

    adicionei para qual stack seria a vaga, qual o nível da vaga, e adicionei mais um for que adicionará o array de ferramentas exigidas pela vaga

--- src/components/input-list/input-list.tsx
+++ src/components/input-list/input-list.tsx
@@ -33,17 +33,36 @@ export const AddJobList = (array: Array<any>, element: HTMLElement) => {
         const creatSection = document.createElement('section');
         creatSection.className = "tags";
         li.appendChild(creatSection);
+
+        //adicionando o item do array denominado "role", onde diz se é a vaga é para front, back, ou fullstack
+        const creatPRole = document.createElement('p');
+        creatPRole.textContent = itemArray.role;
+        creatSection.appendChild(creatPRole)
+
+        //adicionando o nível da vaga (junior, pleno, senior)
+        const creatPLevel = document.createElement('p')
+        creatPLevel.textContent = itemArray.level;
+        creatSection.appendChild(creatPLevel);
         
         //aqui eu adiciono os itens constantes no array language dentro da section criada
         //para cada item será executado um loop
         for (let i = 0; i < itemArray.languages.length; i++) {
-
-            console.log(itemArray.languages[i]);
-            const creatP = document.createElement('p');
-            creatP.textContent = itemArray.languages[i]; //é criado um <p> com o ítem do indice do loop
-            creatSection.appendChild(creatP)
+            console.log(itemArray.languages[i]);        
+            const creatPLanguages = document.createElement('p');            
+            creatPLanguages.textContent = itemArray.languages[i]; //é criado um <p> com o ítem do indice do loop          
+            creatSection.appendChild(creatPLanguages)
 
         }
+
+        //agora um for para adicionar as ferramentas exigidas pela vaga, constantes no campo "tools" do array
+        for (let i = 0; i < itemArray.tools.length; i++) {
+            console.log(itemArray.tools[i]);        
+            const creatPTools = document.createElement('p');            
+            creatPTools.textContent = itemArray.tools[i]; //é criado um <p> com o ítem do indice do loop          
+            creatSection.appendChild(creatPTools);
+
+        }
+
 
 
         //add função verifNew e verifFeatured, que buscará no array constante na data de New e Featured são true ou false
