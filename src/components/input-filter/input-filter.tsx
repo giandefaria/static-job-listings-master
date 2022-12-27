584815ad98e31e6c5d0edf7abdeee084c94be493
Gian <gian.defaria@hotmail.com>
2022-12-27 09:28:28 -0300

    constante filterChildrens captará os elementos filhos de filter, que estão dentro do html. Criei um for para analisar cada classe

--- src/components/input-filter/input-filter.tsx
+++ src/components/input-filter/input-filter.tsx
@@ -1,11 +1,17 @@
 import { filter } from '../main/main'
 
+//função para adicionar um parágrafo no campo filtro. Receberá o parâmetro da TAG e o número do array daquela tag.
+export function createFilter(tags: any, i : number) {
 
-export function createFilter(element: any, i : number) {
-
-    const selectedfilter = document.createElement('p');
-    selectedfilter.textContent = element[i].innerHTML;
-    console.log(element[i].innerHTML);
-    filter.appendChild(selectedfilter);
+    const filterChildrens = filter.children as HTMLCollectionOf<Element>
+    for (let index = 0; index < filterChildrens.length; index++) {
+       
+        
+    }
+    
+    const selectedfilter = document.createElement('p'); //uma const para criar um novo parágrafo
+    selectedfilter.textContent = tags[i].innerHTML;  // o que conterá dentro desse novo parágrafo
+    console.log(tags[i].innerHTML);
+    filter.appendChild(selectedfilter); //esse novo parágrafo será criado dentro do elemento de classe filter
 
 }
