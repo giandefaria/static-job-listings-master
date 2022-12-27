5f40ae627d914ecca449fa25cdcba2ed42c362be
Gian <gian.defaria@hotmail.com>
2022-12-27 11:38:08 -0300

    adicionada imagem  no filtro selecionado

--- src/components/input-filter/input-filter.tsx
+++ src/components/input-filter/input-filter.tsx
@@ -1,4 +1,5 @@
 import { clearAllFilter, filter, filterContent } from '../main/main'
+//import {closeIcon} from '../../assets/icon-remove.svg'
 
 //função para adicionar um parágrafo no campo filtro. Receberá o parâmetro da TAG e o número do índice daquela tag, informações necessárias para especificar qual a tag clicada.
 export function createFilter(tags: any, i : number) {
@@ -11,11 +12,17 @@ export function createFilter(tags: any, i : number) {
     }
     
     if (tagOnList == false) {
+        const createDiv = document.createElement('div')
+        const createButton = document.createElement('button'); //crio o botão com o ícone de excluir a div
+        createButton.className = 'close-button';
         const selectedfilter = document.createElement('p'); //uma const para criar um novo parágrafo
         selectedfilter.textContent = tags[i].innerHTML;  // o que conterá dentro desse novo parágrafo
         selectedfilter.className = 'selected-filter';
         console.log(tags[i].innerHTML);
-        filter.appendChild(selectedfilter); //esse novo parágrafo será criado dentro do elemento de classe filter
+        filter.appendChild(createDiv); //esse nova div será criada dentro do elemento de classe filter
+        createDiv.appendChild(selectedfilter);//esse novo parágrafo será criado dentro da div
+        createDiv.appendChild(createButton);
+        //filter.appendChild(selectedfilter); 
         createClearAllFilterContent();
         
     } else { alert(`Filtro ${tags[i].innerHTML} já adicionado!`) } //se for true, exibo alerta dizendo que o filtro já foi adicionado
