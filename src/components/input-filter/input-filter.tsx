e9841f343ba0d5e740ecf9632f5c8eedde296554
Gian <gian.defaria@hotmail.com>
2022-12-27 09:32:00 -0300

    variável tagOnlist. Por padrão é falsa mas será setada para true se tiver alguma tag igual

--- src/components/input-filter/input-filter.tsx
+++ src/components/input-filter/input-filter.tsx
@@ -2,11 +2,14 @@ import { filter } from '../main/main'
 
 //função para adicionar um parágrafo no campo filtro. Receberá o parâmetro da TAG e o número do array daquela tag.
 export function createFilter(tags: any, i : number) {
+    
+    let tagOnList = false
 
     const filterChildrens = filter.children as HTMLCollectionOf<Element>
     for (let index = 0; index < filterChildrens.length; index++) {
-       
-        
+       if (filterChildrens[index].innerHTML == tags[i].innerHTML) {
+         tagOnList = true
+       }
     }
     
     const selectedfilter = document.createElement('p'); //uma const para criar um novo parágrafo
