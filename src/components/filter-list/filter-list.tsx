3c7df55acc6e460ac69e0d69d4e22fd48edc5dd8
Gian <gian.defaria@hotmail.com>
2022-12-29 23:18:16 -0300

    tentativa de aumentar o número de filtros, mas sem sucesso. Retorno ao código anterior

--- src/components/filter-list/filter-list.tsx
+++ src/components/filter-list/filter-list.tsx
@@ -2,13 +2,17 @@ import { useEffect } from 'react';
 import { AddJobList } from '../input-list/input-list';
 import { jobList } from '../json-file/data'
 
+let linguagem;
 
 export const ResultFilter = jobList.filter(filterlist)
 
 console.log(ResultFilter);
 
+
 export function filterlist(array: any) {
-    return array.languages == "JavaScript"  
+    console.log(array);
+    return array.languages == "JavaScript" ;
+    
 
 };
 
