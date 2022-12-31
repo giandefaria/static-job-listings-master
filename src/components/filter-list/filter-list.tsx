d014f857b9d62c1b2980bbe491597a8b65fb4b75
Gian <gian.defaria@hotmail.com>
2022-12-31 00:31:19 -0300

    tentando montar o caminho de busca das variáveis

--- src/components/filter-list/filter-list.tsx
+++ src/components/filter-list/filter-list.tsx
@@ -1,11 +1,25 @@
 import { clear } from 'console';
-import { useEffect } from 'react';
+import { Key, useEffect } from 'react';
+import { KeyObject } from 'tls';
 import { AddJobList } from '../input-list/input-list';
 import { jobList } from '../json-file/data'
 
 let linguagem = ['JavaScript', 'Python'];
 
+//export const ResultFilter = jobList.filter(filterlist)
 export const ResultFilter = jobList.filter(filterlist)
+console.log(jobList.filter((filtro: any) => {
+    let filtros = filtro.languages;
+
+    for (let index = 0; index < linguagem.length; index++) {
+        filtros = filtros + ` && filtro.languages.includes([${index}])`
+        
+    }
+
+    console.log(filtros);
+    return filtros;
+        
+}));
 
 
 console.log(ResultFilter);
@@ -15,6 +29,7 @@ export function filterlist(array: any) {
     //console.log(array);
     //return array.languages == "JavaScript" ;
     const searchValueInArray = 
+    
         
         array.languages &&
         array.languages.includes(linguagem[0]) && 
